a5a7d5300326b73a2103d383c4aa64655e50922e
chasevanblair <crvanblair@dmacc.edu>
2020-06-12 14:13:41 -0500

    input 1

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -11,20 +11,17 @@ The purpose of this program is to accept test_1 input
 and average it for each student
 
 """
-#from test_1.test_validation_with_try import test_average_exception
 
 
 def average(in_1, in_2, in_3):
-    #scores_input = input("enter the 3 test_1 scores to be averaged separated by comma: ")
-    #final = scores_input.split(",")
     final = [in_1, in_2, in_3]
     score1 = int(final[0])
+    if score1 < 0:
+        raise ValueError
     score2 = int(final[1])
     score3 = int(final[2])
 
     nums = (score1 + score2 + score3) / 3
     return nums
 
-
-#test_average_exception()
 
